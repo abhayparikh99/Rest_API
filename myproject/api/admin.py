0c5b051cf27528668d641caca64945f87610836d
abhayparikh99 <89599000+abhayparikh99@users.noreply.github.com>
2021-09-22 20:21:00 +0530

    Rest_API

--- myproject/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import *
-# Register your models here.
-
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('firstname','lastname','email','city')
-
-admin.site.register(Student,StudentAdmin)
